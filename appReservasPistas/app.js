c4547a040182e7589985a35c4691d440864c7ab3
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-02-06 11:11:19 +0100

    Configuración APP - BBDD

--- appReservasPistas/app.js
+++ appReservasPistas/app.js
@@ -3,25 +3,40 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config();
 
-
+// Enrutado
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usuariosRouter = require('./routes/usuarios');
+const reservasRouter = require('./routes/reservas');
+const pistasRouter = require('./routes/pistas');
+const equipamientosRouter = require('./routes/equipamientos');
+
+// Base de datos
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DB_URI, { 
+  useNewUrlParser: true })
+  .then(() => console.log('connection successful'))
+  .catch((err) => console.error(err));
+mongoose.connection;
 
 const app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+/* app.use(express.json());
+app.use(express.urlencoded({ extended: false })); */
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/usuarios', usuariosRouter);
+app.use('/reservas', reservasRouter);
+app.use('/pistas', pistasRouter);
+app.use('/equipamientos', equipamientosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
