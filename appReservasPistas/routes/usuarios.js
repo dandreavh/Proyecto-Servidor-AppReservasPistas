4c7579ff07fcd9d54b12287548a35fc8ea9255c3
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-02-15 17:49:48 +0100

    API Usuario Final

--- appReservasPistas/routes/usuarios.js
+++ appReservasPistas/routes/usuarios.js
@@ -4,15 +4,6 @@ var mongoose = require('mongoose')
 var Usuario = require('../models/Usuario');
 var db = mongoose.connection;
 
-// POST: Crear un nuevo usuario
-router.post('/', function(req, res, next) {
-  Usuario.create(req.body, function(err, usuario) {
-    console.log(req.body)
-    if (err) res.status(500).send(err);
-    else res.status(200).send({msg:"Usuario creado correctamente", "usuario": usuario});
-  });
-});
-
 // GET: Listar usuarios ordenados por fecha de registro
 router.get('/', function(req, res, next) {
   Usuario.find().sort('-fecha_registro').exec(function(err, usuarios) {
@@ -22,7 +13,7 @@ router.get('/', function(req, res, next) {
 });
 
 // GET: Listar un usuario por su ID (no se muestra ni el id ni el --v)
-router.get('/:id', function(req, res, next) {
+router.get('/find/:id', function(req, res, next) {
   let query = Usuario.findById(req.params.id).select({"_id": 0, "__v":0});
   query.exec(function(err, datosUsuario){
     if (err) res.status(500).send(err);
@@ -31,7 +22,8 @@ router.get('/:id', function(req, res, next) {
 });
 
 // GET: Listar solo los usuarios por su rol
-router.get('/all', function(req, res, next) {
+router.get("/all", function(req, res, next) {
+  console.log(req.query);
   let user_rol = req.query.rol;
   let query = Usuario.find({rol:user_rol}).select({"_id": 0, "__v":0});
   query.exec(function(err, datosUsuario){
@@ -39,6 +31,27 @@ router.get('/all', function(req, res, next) {
     else res.status(200).json(datosUsuario);
   });
 });
+
+// POST: Crear un nuevo usuario
+router.post('/', function(req, res, next) {
+  Usuario.create(req.body, function(err, usuario) {
+    console.log(req.body)
+    if (err) res.status(500).send(err);
+    else res.status(200).send({msg:"Usuario creado correctamente", "usuario": usuario});
+  });
+});
+
+// POST: Logar usuario
+router.post('/login', function(req, res, next) {
+  Usuario.findOne({ email: req.body.email }, function(err, user) {
+    if (err) res.status(500).send(err);
+    if (user != null) {
+      if(user.password == req.body.password){
+        res.status(200).send({ message: 'Bienvenido/a, '+user.nombre});
+      }else res.status(200).send({ message: 'Algún dato es erróneo' });
+    } else res.status(401).send({ message: 'El usuario no existe'});
+  });
+});  
 
 // PUT: Actualizar un usuario por su ID
 router.put('/:id', function(req, res, next) {
