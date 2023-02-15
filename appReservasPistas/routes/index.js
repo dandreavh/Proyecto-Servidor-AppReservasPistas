548a1a4b06d20d558662d3670bd3196ecf0dd020
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-02-15 11:51:45 +0100

    API Usuario V1

--- appReservasPistas/routes/index.js
+++ appReservasPistas/routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mi Pista Ya' });
 });
-
-router.get('/usuarios', (req, res) => {
-  res.render('registro', { title: 'Mi Pista Ya'});
-})
 
 module.exports = router;
